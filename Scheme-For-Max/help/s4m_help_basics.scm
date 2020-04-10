130cc3a4ebe2b93276e4be0abc8d35d1ea9ddf74
Iain Duncan <iduncan@xornot.com>
2020-04-10 09:11:58 -0700

    misc turd polishing

--- Scheme-For-Max/help/s4m_help_basics.scm
+++ Scheme-For-Max/help/s4m_help_basics.scm
@@ -4,6 +4,7 @@
 
 ;; Post to the max console
 (post "Hello world, from S7 scheme!")
+(post "message from scm_help_basic.scm")
 
 ;; Define some variables. 
 ;; ints and floats will keep their type when sent to max
@@ -12,7 +13,7 @@
 (define my-list (list 1 2 3))
 
 ;; post can take all kinds of things (note: no commas)
-(post "some vars: " my-int my-float my-list)
+(post "Some variabless: " my-int my-float my-list)
 
 ;; Define a function that adds 1 to a number, 
 ;; posts the number to the console, and sends it out outlet 0
@@ -20,7 +21,8 @@
     (post "my-fun-1 executing, num is " num)
     (out 0 (+ 1 num)))
 
-;; It's a good idea to have a message at the bottom to let you 
-;; know your source file loaded ok without crashing, at least
-;; until error messages get improved! 
-(post "s4m_help_basics.scm done loading....")
+;; If you edit this file, it's a good idea to have a message at the bottom to let you 
+;; know your source file loaded ok without crashing, at least until error messages get improved! 
+;; (post "s4m_help_basics.scm done loading....")
+
+
