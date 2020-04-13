eb9af1f72bc7b636e83dd571e3a211ea10bea467
Iain Duncan <iduncan@xornot.com>
2020-04-13 11:36:10 -0700

    packaging tweaks

--- make-release.py
+++ make-release.py
@@ -6,17 +6,23 @@ import os
 # its job is to copy the package, including the binary assets and scm files, into the 
 # dist directory, ready for zipping up for release
 
+version = "0.1"     # goes in the tarball
 dry_run = False
 
 externals_src = "../../externals"
 externals_dest = "dist/Scheme-For-Max/externals"
-scm_src = "sm4.scm/scm"
+scm_src = "s4m.scm/scm"
 scm_dest = "dist/Scheme-For-Max/patchers"
+patcher_src = "s4m.scm/patchers"
+patcher_dest = "dist/Scheme-For-Max/patchers"
+
 scm_files = [
     "scm4max.scm",
     "stuff.scm"
 ]
-
+patcher_files = [
+    "s4m.repl.maxpat",
+]
 
 def do(command):
     "wrapper to allow executing dry runs"
@@ -39,11 +45,20 @@ def package_release():
     for f in scm_files:
         do("cp %s/%s %s/%s" % (scm_src, f, scm_dest, f)) 
 
+    print("\n... Copying the max patcher files")
+    for f in patcher_files:
+        do("cp %s/%s %s/%s" % (patcher_src, f, patcher_dest, f)) 
+
+    print("\n... Removing any swap files")
+    do("find dist -type f -name \"*.sw[klmnop]\" -delete")
+
+    #print("\n... Creating zip file")
+    #do("tar cvzf Scheme-For-Max-%s.zip Scheme-For-Max" % version)
 
 if __name__=="__main__":
     print ("\nmake-release.py: package up a scheme-for-max release")
 
-    dry_run = True
+    dry_run = False
     if dry_run:
         print("... Dry run, commands only printed")
     package_release()
