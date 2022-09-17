002f31a5f1ac2ded2b563667c6543de389f32b9f
Iain Duncan <iduncan@xornot.com>
2022-09-16 19:08:56 -0700

    fucking git nonsense

---
+++ s4m/build/CMakeFiles/3.23.0-rc1/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/x86_64/CMakeCXXCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/iainduncan/Documents/Max\ 8/Packages/max-sdk/source/scheme-for-max/s4m/build/CMakeFiles/3.23.0-rc1/CompilerIdCXX/CMakeCXXCompilerId.cpp
---
+++ s4m/build/s4m.build/Debug/s4m.build/Objects-normal/x86_64/s7.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/iainduncan/Documents/Max\ 8/Packages/max-sdk/source/scheme-for-max/s4m/s7.c \
+  /Users/iainduncan/Documents/Max\ 8/Packages/max-sdk/source/scheme-for-max/s4m/mus-config.h \
+  /Users/iainduncan/Documents/Max\ 8/Packages/max-sdk/source/scheme-for-max/s4m/s7.h
